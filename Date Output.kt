568d453e3a78b6f8969df3dd7043736ad2baeaa9
CharkByte <CharkByteDev@gmail.com>
2025-01-09 20:51:10 -0800

    Date Output Program
    A simple Kotlin program that outputs a specified day, using variables and the string interpolation symbol $

---
+++ Date Output.kt
@@ -0,0 +1,18 @@
+// Kotlin Practice, Beginner to Advanced Topics
+/*
+    A simple print statement for the current day,
+    displays a string output with values using the
+    string interpolation symbol $
+*/
+
+fun main() {
+    // Write your code below
+    var todaysDay: Int = 1
+    var todaysMonth: Int = 9
+    var todaysYear: Int = 2025
+
+    /* String interpolation - insert var values into string statements with $  */
+    val todaysDate: String = "$todaysDay/$todaysMonth/$todaysYear"
+
+    println(todaysDate)
+}

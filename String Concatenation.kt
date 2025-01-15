e4833a894da605bc4ef108ea76b3b2ed96c053ca
CharkByte <CharkByteDev@gmail.com>
2025-01-14 23:53:54 -0800

    String Concatenation Program
    A simple program that concatenates strings together to output a sentence

---
+++ String Concatenation.kt
@@ -0,0 +1,18 @@
+/*
+    Kotlin practice concepts, Basic to Advanced
+*/
+
+// A simple string program that concatenates strings together
+// into an output statement.
+
+fun main() {
+    val dog = "Toto"
+    val state = "Kansas"
+    val movie = "The Wizard of Oz"
+
+    val stringConcatenation = dog + ", I've got a feeling we're not in " + state + " anymore. - " + movie + ", 1939"
+
+
+    // Write your code below 🧙
+    print(stringConcatenation)
+}

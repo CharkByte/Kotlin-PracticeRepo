c443ab85c1bc7f67de3bf522a1b52ea36972eb9a
CharkByte <CharkByteDev@gmail.com>
2025-01-14 21:15:19 -0800

    Variable Output Practice
    Practicing basic Kotlin Syntax of variables with integers, boolean and string variables. Outputting these variable declarations to the console terminal.

---
+++ Variable Output Practice.kt
@@ -0,0 +1,25 @@
+fun main() {
+    // Kotlin Practice, Beginner to Advanced Topics
+    /*
+         A simple print statement, declaring and initializing different variable
+         types in the Kotlin syntax. String, Boolean and Integer are being output
+         to the console.
+     */
+
+    // Variable Declaration and Initializations
+    var stringSentence: String = "String Sentence"
+    var booleanType: Boolean = true
+    var intType: Int = 45
+
+    // Declaration of variable name and type without an initialization statement
+    var intAlso: Int
+
+    // Variable initialization with a value
+    intAlso = 100
+
+    // Console print output of all variables
+    println(stringSentence)
+    println(booleanType)
+    println(intType)
+    println(intAlso)
+}

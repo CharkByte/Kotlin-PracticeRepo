64a2db88dbeffdc05612cc76bb0adcce41cadcf3
CharkByte <CharkByteDev@gmail.com>
2025-01-25 02:52:59 -0800

    User Input Practice Program
    Printing user input statements, using readline and print statements

---
+++ User Input Practice 1.kt
@@ -0,0 +1,12 @@
+
+// Kotlin practice concepts, Basic to Advanced
+/*
+   User input and output practice with readline and printline statements.
+*/
+
+fun main(){
+    print ("Enter a statement: ")
+
+    val statement = readLine();
+    println("Your statement was: $statement")
+}

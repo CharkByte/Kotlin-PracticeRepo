4a1023558ef135c9f1e00c7ea338b5dce902d65d
CharkByte <CharkByteDev@gmail.com>
2025-01-26 21:57:23 -0800

    Kt String Function Practice Program
    Using innate functions from the string library in Kotlin. Using functions such as  .length or .capitalize( ) to manipulate string data and output to screen.

---
+++ Kt String Functions 1.kt
@@ -0,0 +1,16 @@
+
+// Kotlin practice concepts, Basic to Advanced
+/*
+   Kotlin string library functions. Using capitalize & length check functions
+*/
+
+fun main() {
+    val number = "twenty four"
+
+    println("// Kotlin string library function that checks length of string:")
+    println( " '$number' is " + number.length + " characters long")
+    println("// Kotlin string library function that capitalizes first char of string:")
+    println(number.capitalize())
+    println("// Original string data, remains unchanged:")
+    print(number)
+}

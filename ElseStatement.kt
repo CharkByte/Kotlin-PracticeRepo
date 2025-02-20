f88edfb1be10876f12f157b3d50b05013ddb27a0
CharkByte <CharkByteDev@gmail.com>
2025-02-20 01:04:33 -0800

    Else Statement Program
    A simple print output that displays different outputs based on the else statement

---
+++ ElseStatement.kt
@@ -0,0 +1,22 @@
+// Kotlin practice concepts, Basic to Advanced
+/*
+   ElseStatement.kt
+   -------------------------------------------------------------------
+   A program that has an else statement, switches between two output
+   statements based on boolean logic and whether the price is high or
+   not.
+*/
+
+fun main (){
+    var priceHigh = false
+    var price: Int
+
+    if (priceHigh){
+
+        price = 10000
+    }
+    else{
+        price = 1
+    }
+    println("The price is $price")
+}
